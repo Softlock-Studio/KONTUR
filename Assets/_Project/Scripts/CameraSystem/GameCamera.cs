d3354a0265ca0d6940244968664cf4bd37b9ee1c
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2026-07-21 22:35:47 +0300

    Added camera system
    Added MVP
    Added Game Camera prefab
    Changed Display Canvas prefab accordingly

---
+++ Assets/_Project/Scripts/CameraSystem/GameCamera.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace CameraSystem
+{
+    [RequireComponent(typeof(Collider))]
+    public class GameCamera : MonoBehaviour
+    {
+        [SerializeField] private Camera _correspondingCamera;
+        [SerializeField] private string _localisationKey;
+        private int _cameraID;
+
+        private void Awake()
+        {
+            _cameraID = gameObject.GetInstanceID();
+        }
+        public int GetCameraID() => _cameraID;
+        public string GetLocalisationKey() => _localisationKey;
+        public void TurnOffCamera()
+        {
+            _correspondingCamera.targetTexture = null;
+        }
+
+        public void TurnOnCamera(RenderTexture cameraTexture)
+        {
+            _correspondingCamera.targetTexture = cameraTexture;
+        }
+    }
+}
