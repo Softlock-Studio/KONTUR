0c02c52270140e734b2dae3ca6413d6cd2adef20
Allan <actually.star@gmail.com>
2026-08-02 00:00:20 +0300

    Added sound and sprites to cameras

--- Assets/_Project/Scripts/CameraSystem/GameCamera.cs
+++ Assets/_Project/Scripts/CameraSystem/GameCamera.cs
@@ -1,6 +1,10 @@
 using CameraSystem.Rendering;
+using Game.Audio;
+using Game.Bootstrap;
 using Game.House;
 using UnityEngine;
+using VContainer;
+using VContainer.Unity;
 
 namespace CameraSystem
 {
@@ -10,12 +14,23 @@ namespace CameraSystem
         [SerializeField] private Camera _correspondingCamera;
         [SerializeField] private string _localisationKey;
         [SerializeField] private Zone _observedZone;
+        [SerializeField] private SpriteRenderer _iconRenderer;
+        [SerializeField] private Sprite _defaultSprite;
+        [SerializeField] private Sprite _selectedSprite;
+        [SerializeField] private SfxCue _clickCue;
+
         private int _cameraID;
+        private IAudioService _audioService;
 
         private void Awake()
         {
             _cameraID = gameObject.GetInstanceID();
         }
+        private void Start()
+        {
+            _audioService = LifetimeScope.Find<GameLifetimeScope>().Container.Resolve<IAudioService>();
+        }
+
         public int GetCameraID() => _cameraID;
         public string GetLocalisationKey() => _localisationKey;
         public Zone GetObservedZone() => _observedZone;
@@ -23,12 +38,16 @@ namespace CameraSystem
         {
             _correspondingCamera.targetTexture = null;
             _correspondingCamera.enabled = false;
+            _iconRenderer.sprite = _defaultSprite;
         }
 
         public void TurnOnCamera(RenderTexture cameraTexture)
         {
             _correspondingCamera.targetTexture = cameraTexture;
             _correspondingCamera.enabled = true;
+            _iconRenderer.sprite = _selectedSprite;
+            _audioService.PlayUiSfx(_clickCue);
+
         }
 
         public bool TrySetNoiseBlend(float blend)
